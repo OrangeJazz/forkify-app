a00b43ba8f6173ba5c6a1f6098eddec39bb4d187
OrangeJazz <wilinwen@mail.ru>
2023-02-07 21:46:35 +0400

    feat: recipe render with MVC

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,3 +1,9 @@
+import * as model from './model';
+import recipeView from './views/recipeView';
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -11,3 +17,23 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    // 1) loading recipe
+    await model.loadRecipe(id);
+
+    // 2) rendering recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+['hashchange', 'load'].forEach(event =>
+  window.addEventListener(event, controlRecipes)
+);
