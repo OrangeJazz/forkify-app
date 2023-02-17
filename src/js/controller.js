19284c993a4bdd569818169ffa1e63f2c46afd6f
OrangeJazz <wilinwen@mail.ru>
2023-02-17 17:06:04 +0400

    refactor: remove handler to view from controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -26,6 +26,8 @@ const controlRecipes = async function () {
   }
 };
 
-['hashchange', 'load'].forEach(event =>
-  window.addEventListener(event, controlRecipes)
-);
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+};
+
+init();
