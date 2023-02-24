19170a4de0d8dc2167337d2a8f26b59abc18a7a7
OrangeJazz <wilinwen@mail.ru>
2023-02-24 20:35:09 +0400

    refactor: delete some comments

--- src/js/config.js
+++ src/js/config.js
@@ -2,4 +2,4 @@ export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 3;
 export const RES_PER_PAGE = 10;
 export const KEY = '0fb0c1ca-9bea-480f-92d4-9cb3f33ac6e0';
-export const MODAL_CLOSE_TIME = 2.5;
+export const MODAL_CLOSE_TIME = 1;
